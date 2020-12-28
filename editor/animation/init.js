003f3f882712b54e6487778efe74535c1aa0411e
kurosawa4434 <kurosawa4434@gmail.com>
2020-12-28 22:56:05 +0900

    fix multipleArguments -> true

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -119,7 +119,7 @@ requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
         var $tryit;
 
         var io = new extIO({
-            multipleArguments: false,
+            multipleArguments: true,
             functions: {
                 python: 'park_benches',
                 js: 'parkBenches'
